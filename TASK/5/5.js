9340fef4474ff369a4036d1baec8bd56258ebc4a
Dhanush001-Devil <dhanushraja02@gmail.com>
2025-04-23 11:19:19 +0530

    Initial commit

---
+++ TASK/5/5.js
@@ -0,0 +1,16 @@
+const toggleButton = document.getElementById("toggle-theme");
+
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  
+  const isDark = document.body.classList.contains("dark-mode");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+});
